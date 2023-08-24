25ae7c8726e72a46e53b90544167563b6e10e6d6
RosalesJhordi <1428060@senati.pe>
2023-08-24 15:25:14 -0500

    Añadi ELSE y Do-While

--- Seleccion.java
+++ Seleccion.java
@@ -3,54 +3,62 @@ import java.util.Scanner;
 public class Seleccion {
 
     public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        int op;
+        do {
+            Scanner sc = new Scanner(System.in);
+            int op;
 
-        // Arreglo de pasteles con sus precios
-        Cake[] cakes = {
-            new Cake("Pastel Tres Leches", 15),
-            new Cake("Pastel de Chocolate", 8),
-            new Cake("Pastel de Vainilla", 6),
-            new Cake("Pastel de Fresa", 2)
-        };
+            // Arreglo de pasteles con sus precios
+            Cake[] cakes = {
+                    new Cake("Pastel Tres Leches", 85),
+                    new Cake("Pastel de Chocolate", 60),
+                    new Cake("Pastel de Vainilla", 65),
+                    new Cake("Pastel de Fresa", 55)
+            };
 
-        System.out.print("\n\tPasteleria\n\nIngresa su opcion:\n\n1:Pastel mas barto\n2:Pastel mas caro\n\nOpcion: ");
-        op = sc.nextInt();
+            System.out.print(
+                    "\n\tPasteleria\n\nIngresa su opcion:\n\n1:Pastel mas barto\n2:Pastel mas caro\n\n\tCualquier numero para salir\n\nOpcion: ");
+            op = sc.nextInt();
 
-        if (op == 1) {
+            if (op == 1) {
 
-            // Encontrar el pastel más barato
-            Cake barato = cakes[0];
+                // Encontrar el pastel más barato
+                Cake barato = cakes[0];
 
-            //bucle for que recorre la matris cake desde indice 1 al final 
-            for (int i = 1; i < cakes.length; i++) {
+                // bucle for que recorre la matris cake desde indice 1 al final
+                for (int i = 1; i < cakes.length; i++) {
 
-                // En cada iteración del bucle, se compara el precio del pastel actual
-                if (cakes[i].getPrice() < barato.getPrice()) { 
-                    barato = cakes[i];
+                    // En cada iteración del bucle, se compara el precio del pastel actual
+                    if (cakes[i].getPrice() < barato.getPrice()) {
+                        barato = cakes[i];
+                    }
                 }
-            }
-            // Imprimir el resultado
-            System.out.println("\nEl pastel mas barato es: " + barato.getName() +
-                    " con un precio de " + barato.getPrice());
-        } else if (op == 2) {
-            // Encontrar el pastel más caro
-            Cake caro = cakes[0];
+                // Imprimir el resultado
+                System.out.println("\nEl pastel mas barato es: " + barato.getName() +" con un precio de " + barato.getPrice() + " Soles");
+            } else if (op == 2) {
+                // Encontrar el pastel más caro
+                Cake caro = cakes[0];
 
-            for (int i = 1; i < cakes.length; i++) {
+                for (int i = 1; i < cakes.length; i++) {
 
-                if (cakes[i].getPrice() > caro.getPrice()) { // 
-                    caro = cakes[i];
+                    if (cakes[i].getPrice() > caro.getPrice()) { //
+                        caro = cakes[i];
+                    }
                 }
+                // Imprimir el resultado
+                System.out
+                        .println("\nEl pastel mas caro es: " + caro.getName() + " con un precio de " + caro.getPrice() + " Soles");
+            } else {
+                System.out.println("Opcion No valida.....Saliendo");
+                break;
             }
-            // Imprimir el resultado
-            System.out.println("\nEl pastel mas caro es: " + caro.getName() +" con un precio de " + caro.getPrice());
-        }
+        } while (10 == 10);
     }
 }
 
+
 class Cake {
+    
+    // Representamos objetos y cada objeto Cake tiene dos propiedades
     private String name;
     private int price;
 
