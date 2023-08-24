96c48d780b6827a19018bb6511d00bf76b771d02
RosalesJhordi <1428060@senati.pe>
2023-08-24 15:12:06 -0500

    Creamos un arreglo con los pasteles - creamos la clase Cake y usamos el get para optener los nombres

--- Seleccion.java
+++ Seleccion.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 public class Seleccion {
 
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        int op;
+
+        // Arreglo de pasteles con sus precios
+        Cake[] cakes = {
+            new Cake("Pastel Tres Leches", 15),
+            new Cake("Pastel de Chocolate", 8),
+            new Cake("Pastel de Vainilla", 6),
+            new Cake("Pastel de Fresa", 2)
+        };
+    }
+}
+
+class Cake {
+    private String name;
+    private int price;
+
+    public Cake(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
